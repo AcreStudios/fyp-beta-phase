5f5e59ea4e879be7bc8445e7ad7e960946730f2b
Gavin <goldenwarrior97@gmail.com>
2016-09-14 00:04:31 +0800

    Menu Imported!
    Imported menu and fixed a bug with the video.
    TO be done:
    - Linkage from menu to levels

---
+++ FYP BETA PHASE/Assets/Menu/Scripts/Keyboard Controller Scripts/CreditsMenuControlScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsMenuControlScript : MonoBehaviour {
+
+    public NewMenuScript newMenuScript;
+    int selectedIndex = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("return")) {
+            if (selectedIndex == 0) {
+                newMenuScript.SelectBack();
+            }
+        }
+        if(Input.GetKeyDown("escape")) {
+            newMenuScript.SelectBack();
+        }
+    }
+}
