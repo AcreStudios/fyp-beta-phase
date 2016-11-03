ac2b9ce1dcbb70d369e274cc5d02ee456e17b003
zlurker <tallspin_97@hotmail.com>
2016-11-03 18:08:39 +0800

    Folder Shifting
    Shift my scripts into general scripts folder

---
+++ FYP BETA PHASE/Assets/Scripts/ExperimentalScripts(Gab)/ColliderExperiments.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderExperiments : MonoBehaviour {
+
+    [System.Serializable]
+    public struct BoundaryPoints {
+        public Vector3[] centrePoints;
+        public Vector3[] outerPoints;
+    }
+
+    public BoundaryPoints testPoints;
+    public Collider[] testColl;
+
+    void Start() {
+        
+        //testMesh = testColl.sharedMesh;
+    }
+
+    void Update() {
+    }
+}
