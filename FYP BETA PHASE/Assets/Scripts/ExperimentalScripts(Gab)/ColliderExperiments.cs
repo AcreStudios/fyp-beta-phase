8d31ae7469490c85ee0764df9ee3bfef7cccd17b
zlurker <tallspin_97@hotmail.com>
2016-11-05 23:01:20 +0800

    Enhancement to collider reader
    -Made enhancements to it. Much more stable now.
    -Missing abit of "smartness". If the collider that is first selected for
    reading is in a middle of collision it will cause problem. Will patch it
    soon.
    -Next update will feature reading of colliders which has "Obstacle" tag,
    and possibly AI's being able to access the data obtained from the
    reading.

--- FYP BETA PHASE/Assets/Scripts/ExperimentalScripts(Gab)/ColliderExperiments.cs
+++ FYP BETA PHASE/Assets/Scripts/ExperimentalScripts(Gab)/ColliderExperiments.cs
@@ -6,7 +6,7 @@ public class ColliderExperiments : MonoBehaviour {
 
     [System.Serializable]
     public struct BoundaryPoints {
-        public Vector3[] centrePoints;
+        public Vector3 centrePoint;
         public List<Vector3> outerPoints;
     }
 
@@ -15,80 +15,87 @@ public class ColliderExperiments : MonoBehaviour {
     public Vector3[] multiplier;
     public int orientIndex;
 
-    Vector3 prevVector = Vector3.zero;
+    /*Vector3 prevVector = Vector3.zero;
     Vector3 nextVector = Vector3.zero;
 
     Vector3 modifiedExtents = Vector3.zero;
     RaycastHit hit;
-    Vector3 oringinalPos;
+    Collider originalColl;
     float timer;
+    bool first;*/
 
             
 
     void Start() {
-        orientIndex = 0;
-
-        testPoints.outerPoints = new List<Vector3>();
-
-        modifiedExtents = new Vector3(testColl.bounds.extents.x * multiplier[orientIndex].x, testColl.bounds.extents.y * multiplier[orientIndex].y, testColl.bounds.extents.z * multiplier[orientIndex].z);
-        oringinalPos = testColl.bounds.center + modifiedExtents;
-        prevVector = oringinalPos;
-        prevVector.y = testColl.bounds.center.y;
-        //ReadColliders(testColl,testColl)
-        //testMesh = testColl.sharedMesh;
-        /*Vector3 prevVector = Vector3.zero;
-        Vector3 nextVector = Vector3.zero;
-
-        Vector3 modifiedExtents = Vector3.zero;
-        RaycastHit hit;
-
-        modifiedExtents = new Vector3(testColl.bounds.extents.x * multiplier[orientIndex].x, testColl.bounds.extents.y * multiplier[orientIndex].y, testColl.bounds.extents.z * multiplier[orientIndex].z);
-        Vector3 oringinalPos = testColl.bounds.center + modifiedExtents;
-        prevVector = oringinalPos;
-
-        while (nextVector != oringinalPos) {
-            orientIndex = AddIndex(orientIndex, 1, multiplier.Length);
-            modifiedExtents = new Vector3(testColl.bounds.extents.x * multiplier[orientIndex].x, testColl.bounds.extents.y * multiplier[orientIndex].y, testColl.bounds.extents.z * multiplier[orientIndex].z);
-            nextVector = testColl.bounds.center + modifiedExtents;
-
-            if (Physics.Linecast(prevVector, nextVector, out hit)) {
-                if (hit.collider != testColl) {
-                    testColl = hit.collider;
-                    nextVector = hit.point;
-                    orientIndex = AddIndex(orientIndex, -1, multiplier.Length);
-                }
-            }
-
-            Debug.DrawLine(prevVector, nextVector, Color.red, 20);
-            testPoints.outerPoints.Add(nextVector);
-
-            prevVector = nextVector;
-        }*/
+        ReadColliders();
     }
 
     void Update() {
-        if (nextVector != oringinalPos && Time.time > timer) {
+        /*if (testColl != originalColl || orientIndex != 0 && Time.time > timer) {
+            //first = false;
             orientIndex = AddIndex(orientIndex, 1, multiplier.Length);
             modifiedExtents = new Vector3(testColl.bounds.extents.x * multiplier[orientIndex].x, testColl.bounds.extents.y * multiplier[orientIndex].y, testColl.bounds.extents.z * multiplier[orientIndex].z);
             nextVector = testColl.bounds.center + modifiedExtents;
             nextVector.y = testColl.bounds.center.y;
+
+            testColl.gameObject.layer = 2;
+
             if (Physics.Linecast(prevVector, nextVector, out hit)) {
-                if (hit.collider != testColl) {
-                    Debug.Log("Working");
+                    testColl.gameObject.layer = 0;
                     testColl = hit.collider;
                     nextVector = hit.point;
                     orientIndex = AddIndex(orientIndex, -1, multiplier.Length);
                     orientIndex = AddIndex(orientIndex, -1, multiplier.Length);
-                }
             }
 
-            Debug.DrawLine(prevVector, nextVector, Color.red);
+            Debug.DrawLine(prevVector, nextVector, Color.red,60);
+            Debug.Log("Working");
             testPoints.outerPoints.Add(nextVector);
 
             prevVector = nextVector;
             
             timer = Time.time + 1;
-        }
+        }*/
+    }
+
+    void ReadColliders() {
+        Vector3 prevVector = Vector3.zero;
+        Vector3 nextVector = Vector3.zero;        
+        Vector3 modifiedExtents = Vector3.zero;
+        RaycastHit hit;
+        Collider originalColl;
+
+        orientIndex = 0;
+
+        testPoints.outerPoints = new List<Vector3>();
+        modifiedExtents = new Vector3(testColl.bounds.extents.x * multiplier[orientIndex].x, testColl.bounds.extents.y * multiplier[orientIndex].y, testColl.bounds.extents.z * multiplier[orientIndex].z);
+        
+        prevVector = testColl.bounds.center + modifiedExtents;
+        prevVector.y = testColl.bounds.center.y;
+
+        originalColl = testColl;
+        testColl.gameObject.layer = 2;
+
+        do {
+            orientIndex = AddIndex(orientIndex, 1, multiplier.Length);
+            modifiedExtents = new Vector3(testColl.bounds.extents.x * multiplier[orientIndex].x, testColl.bounds.extents.y * multiplier[orientIndex].y, testColl.bounds.extents.z * multiplier[orientIndex].z);
+            nextVector = testColl.bounds.center + modifiedExtents;
+            nextVector.y = testColl.bounds.center.y;
+
+            if (Physics.Linecast(prevVector, nextVector, out hit)) {
+                testColl.gameObject.layer = 0;
+                testColl = hit.collider;
+                testColl.gameObject.layer = 2;
+                nextVector = hit.point;
+                orientIndex = AddIndex(orientIndex, -1, multiplier.Length);
+                orientIndex = AddIndex(orientIndex, -1, multiplier.Length);
+            }
+
+            Debug.DrawLine(prevVector, nextVector, Color.red, Mathf.Infinity);
+            testPoints.outerPoints.Add(nextVector);
+
+            prevVector = nextVector;
+        } while (testColl != originalColl || orientIndex != 0);
     }
 
     int AddIndex(int currentValue, int value, int arrayCount) {
