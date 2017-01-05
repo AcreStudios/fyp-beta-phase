0b8cd6f9ad54822afa56e0feb41b9d2d0b5f18bf
Aweife <aweifeisaweife@gmail.com>
2017-01-05 14:40:27 +0800

    Health Regen
    Done:
    - Two modes of regen: One Shot & Over Time

--- FYP BETA PHASE/Assets/Scripts/Character/Health.cs
+++ FYP BETA PHASE/Assets/Scripts/Character/Health.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour 
 {
+	public enum HealthRegen { ONESHOT, OVERTIME }
+
 	// Components
 	private CharacterController characterController;
 	private RagdollHandler ragdollHandler;
@@ -23,6 +25,7 @@ public class Health : MonoBehaviour
 
 	[Header("------Only Applies To Player------")]
 	public bool isPlayer = false;
+
 	[Header("-Death Effect-")]
 	public UnityStandardAssets.ImageEffects.Grayscale grayScreenEffect;
 	public float fadeScreenDelay = 3f;
@@ -30,26 +33,43 @@ public class Health : MonoBehaviour
 	public float reloadSceneDelay = 1f;
 	public int sceneToLoadInt;
 	public string sceneToLoadName;
+
 	[Header("-Screen Flash When Damaged-")]
-	public Color BloodSplatterShowColor = new Color(1f, 1f, 1f, .75f);
-	public Color BloodSplatterFadeColor = new Color(1f, 1f, 1f, 0f);
-	public Color ShowBloodTintColor = new Color(1f, 1f, 1f, 1f);
-	public Color CurrentBloodTintColor = new Color(1f, 1f, 1f, 0f);
-	public float DamageFlashSpeed = 3f;
-	public UnityEngine.UI.Image CurrentBloodSpatterImage;
-	public UnityEngine.UI.Image CurrentBloodTintImage;
+	public UnityEngine.UI.Image BloodSpatterImage;
+	public UnityEngine.UI.Image RedTintImage;
 	public UnityEngine.Sprite[] BloodSpatterSprites;
+	[Range(0f, 1f)]
+	public float BloodSplatterFlashTransparency = .75f, RedTintFlashTransparency = 1f, RedTintStayIntensity = .5f;
+	public float DamageFlashSpeed = 3f;
 
-	private bool damaged = false;
+	private Color currentRedTintTransparency;
+
+	[Header("-Health Regeneration-")]
+	public HealthRegen HealthRegenMode;
+
+	[Header("One Shot")]
+	public float TriggerOneShotRegenDelay = 5f;
+	public float OneShotRecoverSpeed = 40f;
+
+	private float triggerOneShotRegenTimer;
+	private bool oneShotRegenerating = false;
+
+	[Header("Over Time")]
+	public float OverTimeRecoverSpeed = 10f;
+	[Range(0f, 1f)]
+	public float BaseOverTimeRedTintFlash = .5f;
 
 
-	private void Awake()
+	void Awake()
 	{
 		characterController = GetComponent<CharacterController>();
 		ragdollHandler = GetComponentInChildren<RagdollHandler>();
+
+		// Initialise
+		currentRedTintTransparency = new Color(1f, 1f, 1f, 0f);
 	}
 
-	void Update() 
+	void Update()
 	{
 		// Debug in inspector only
 		if(debugDeath)
@@ -59,7 +79,7 @@ public class Health : MonoBehaviour
 			curHealth = 0f;
 			Die();
 		}
-		
+
 		if(DebugDamage)
 		{
 			DebugDamage = false;
@@ -67,16 +87,81 @@ public class Health : MonoBehaviour
 			ReceiveDamage();
 		}
 
-		// Fade out screen damage flash
-		CurrentBloodSpatterImage.color = Color.Lerp(CurrentBloodSpatterImage.color, new Color(1f, 1f, 1f, 0f), DamageFlashSpeed * Time.deltaTime);
-		CurrentBloodTintImage.color = Color.Lerp(CurrentBloodTintImage.color, CurrentBloodTintColor, DamageFlashSpeed * Time.deltaTime);
+		// Update damage feedback
+		LerpTargetColor();
+
+		// Health regeneration
+		curHealth = Mathf.Clamp(curHealth, 0f, 100f);
+
+		switch(HealthRegenMode)
+		{
+			case HealthRegen.ONESHOT:
+				OneShotHealthRegen();
+				break;
+			case HealthRegen.OVERTIME:
+				OverTimeRegen();
+				break;
+		}
 	}
 
-	public void ReceiveDamage(float dmg = 5f)
+	private void LerpTargetColor() // Continuosly lerp to target color 
 	{
+		if(!BloodSpatterImage || !RedTintImage) return;
+
+		currentRedTintTransparency.a = 0f + (1f - (curHealth / 100f)) * RedTintStayIntensity;
+
+
+		BloodSpatterImage.color = Color.Lerp(BloodSpatterImage.color, Color.clear, DamageFlashSpeed * Time.deltaTime);
+		RedTintImage.color = Color.Lerp(RedTintImage.color, currentRedTintTransparency, DamageFlashSpeed * Time.deltaTime);
+	}
+
+	private void OneShotHealthRegen()
+	{
+		// If health is not full
+		if(curHealth > 0 && curHealth < 100f)
+		{
+			// Run timer
+			if(triggerOneShotRegenTimer > 0f)
+				triggerOneShotRegenTimer -= Time.deltaTime;
+
+			// Check timer
+			if(triggerOneShotRegenTimer <= 0f && !oneShotRegenerating)
+				StartCoroutine(RegenToFullHealth());
+		}
+	}
+
+	private IEnumerator RegenToFullHealth() // One shot regen to full health 
+	{
+		oneShotRegenerating = true;
+
+		while(curHealth < 100f)
+		{
+			curHealth += OneShotRecoverSpeed * Time.deltaTime;
+			yield return null;
+		}
+
+		curHealth = 100f;
+		oneShotRegenerating = false;
+	}
+
+	private void OverTimeRegen()
+	{
+		// If health is not full
+		if(curHealth > 0 && curHealth < 100f)
+			curHealth += OverTimeRecoverSpeed * Time.deltaTime;
+	}
+
+	public void ReceiveDamage(float dmg = 15f)
+	{
+		if(oneShotRegenerating) return;
+
 		curHealth -= dmg;
 
+		// Damage feedback
 		FlashScreenOnDamage();
+
+		// Reset oneshot regeneration timer
+		triggerOneShotRegenTimer = TriggerOneShotRegenDelay;
 
 		// If no health, die
 		if(curHealth <= 0)
@@ -85,21 +170,29 @@ public class Health : MonoBehaviour
 
 	private void FlashScreenOnDamage()
 	{
-		if(BloodSpatterSprites.Length == 0) return;
+		if(BloodSpatterSprites.Length == 0 || !BloodSpatterImage || !RedTintImage) return;
 
-		damaged = true;
+		// Blood Splatter
+		BloodSpatterImage.sprite = BloodSpatterSprites[Random.Range(0, BloodSpatterSprites.Length)];
+		BloodSpatterImage.color = new Color(1f, 1f, 1f, BloodSplatterFlashTransparency);
 
-		CurrentBloodSpatterImage.sprite = BloodSpatterSprites[Random.Range(0, BloodSpatterSprites.Length)];
-		CurrentBloodSpatterImage.color = BloodSplatterShowColor;
-		CurrentBloodTintImage.color = ShowBloodTintColor;
-		CurrentBloodTintColor.a = 0f + (1f - (curHealth / 100f));
-}
+		// Red Tint
+		switch(HealthRegenMode)
+		{
+			case HealthRegen.ONESHOT:
+				RedTintImage.color = new Color(1f, 1f, 1f, RedTintFlashTransparency);
+				break;
+			case HealthRegen.OVERTIME:
+				RedTintImage.color = new Color(1f, 1f, 1f, (Mathf.Abs(((curHealth / 100f) - 1f)) * .8f) + .5f);
+				break;
+		}
+	}
 
 	private void Die()
 	{
 		characterController.enabled = false;
-		CurrentBloodSpatterImage.color = new Color(0f, 0f, 0f, 0f);
-		CurrentBloodTintImage.color = new Color(0f, 0f, 0f, 0f);
+		BloodSpatterImage.color = new Color(0f, 0f, 0f, 0f);
+		RedTintImage.color = new Color(0f, 0f, 0f, 0f);
 
 		if(ragdollHandler)
 			ragdollHandler.BecomeRagdoll();
